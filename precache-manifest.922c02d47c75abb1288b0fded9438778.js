ebc1bb72dfde00cd9731718f7b721f1f8d24682f
ginnnnnn <aginlo@hotmail.com>
2019-07-04 22:16:16 +0800

    Updates

---
+++ precache-manifest.922c02d47c75abb1288b0fded9438778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f810ac82264c39ac8feeeb4cfa65df47",
+    "url": "/Booklist-ReactHook-reducer-theNetNinja-notes/index.html"
+  },
+  {
+    "revision": "8734388c86ab817ce3e4",
+    "url": "/Booklist-ReactHook-reducer-theNetNinja-notes/static/css/main.719106ed.chunk.css"
+  },
+  {
+    "revision": "9445687158d5b88835e3",
+    "url": "/Booklist-ReactHook-reducer-theNetNinja-notes/static/js/2.44afff6c.chunk.js"
+  },
+  {
+    "revision": "8734388c86ab817ce3e4",
+    "url": "/Booklist-ReactHook-reducer-theNetNinja-notes/static/js/main.c5c4bda1.chunk.js"
+  },
+  {
+    "revision": "4bc7fd466d715ad7242d",
+    "url": "/Booklist-ReactHook-reducer-theNetNinja-notes/static/js/runtime~main.51175138.js"
+  }
+]);
